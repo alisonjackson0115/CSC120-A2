782a3a646092cb5263c4bbb3739ceb3609234e55
alisonjackson0115 <ajackson25@smith.edu>
2025-05-09 13:40:50 -0400

    first push

--- computer.py
+++ computer.py
@@ -2,6 +2,12 @@ class Computer:
 
     # What attributes will it need?
 
+    # storing information about a specific computer
+    # updating a computer's price
+    # updating a computer's OS
+    # refurbishing a computer
+
+
     # How will you set up your constructor?
     # Remember: in python, all constructors have the same name (__init__)
     def __init__():
--- oo_resale_shop.py
+++ oo_resale_shop.py
@@ -2,6 +2,12 @@ class ResaleShop:
 
     # What attributes will it need?
 
+    # storing the inventory for the store
+    # buying a computer (add to inventory)
+    #  selling a computer (remove from inventory)
+
+
+
     # How will you set up your constructor?
     # Remember: in python, all constructors have the same name (__init__)
     def __init__():
